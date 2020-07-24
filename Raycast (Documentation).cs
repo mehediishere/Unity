a075673a1f2ed735177ae0dba748b323b78196de
Mehedi Hasan <51486542+mehediishere@users.noreply.github.com>
2020-07-24 22:50:44 +0600

    Update Raycast (Documentation).cs

--- Raycast (Documentation).cs
+++ Raycast (Documentation).cs
@@ -15,4 +15,17 @@
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * unableToDetectAtDistance, Color.white);
                 Debug.Log("Did not Hit");
             }
+                
+            ///////////////////////////////////////////////
+            
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 20) && hit.collider.gameObject.CompareTag("Object Tag Name Here"))
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20.0f, Color.red);
+                    Debug.Log("Did Hit");
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20.0f, Color.white);
+                    Debug.Log("Did not Hit");
+                }
         }
