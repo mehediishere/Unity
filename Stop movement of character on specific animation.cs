307ff27310a399a45fa8fc706c4e2a46632c2ef0
Mehedi Hasan <51486542+mehediishere@users.noreply.github.com>
2020-08-27 17:00:46 +0600

    Create Stop movement of character on specific animation.cs

---
+++ Stop movement of character on specific animation.cs
@@ -0,0 +1,9 @@
+ //  on your player controller have a reference to your animator
+ //  and stick this code before any movement is preformed
+ //  @aaron_kotch
+ 
+ 
+ if(anim.GetCurrentAnimatorStateInfo(0).IsName("The name of your animation clip you want to freeze upon")
+ {
+       return;
+ }
