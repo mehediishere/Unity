670fe15c0ee6699b12ed4a98aeb4c33264589fb8
Mehedi Hasan <51486542+mehediishere@users.noreply.github.com>
2020-11-02 13:19:00 +0600

    Update combo attack.cs

--- Combo Attack/combo attack.cs
+++ Combo Attack/combo attack.cs
@@ -1,73 +1,76 @@
-//Every attack animation need event function according to steps shown
-
 using UnityEngine;
- 
-public class combo : MonoBehaviour {
-       
+
+public class combo : MonoBehaviour
+{
     Animator anim;
-       
     int noOfClicks; //Determines Which Animation Will Play
-    bool canClick; //Locks ability to click during animation event
-     
+    bool canClick; //Locks ability to click during Animation event
+    public string attack1, attack2, attack3;
+
+
     void Start()
-    {     
+    {
         //Initialize appropriate components
-        anim = GetComponent<Animator>();      
-               
+        anim = GetComponent<Animator>();
+
         noOfClicks = 0;
         canClick = true;
     }
-         
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) { ComboStarter(); }
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            ComboStarter(); 
+        }
+        //Debug.Log(noOfClicks);
     }
- 
+
     void ComboStarter()
-    {      
-        if (canClick)           
-        {           
+    {
+        if (canClick)
+        {
             noOfClicks++;
         }
-                
+
         if (noOfClicks == 1)
-        {           
-            anim.SetInteger("animation", 1);
-        }          
-    }
-     
-    //Note: "ComboCheck()" will be added in animation event
-    public void ComboCheck() {     
-        
-        canClick = false;
-        
-        if(anim.GetCurrentAnimatorStateInfo(0).IsName("1st attack name") && noOfClicks == 1 ) //If the first animation is still playing and only 1 click has happened, return to idle
         {
-            anim.SetInteger("animation", 4);
-            canClick = true;
-            noOfClicks = 0;
+            anim.SetInteger("ComboState", 1);
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("1st attack name") &&  noOfClicks >= 2) //If the first animation is still playing and at least 2 clicks have happened, continue the combo 
-        {         
-            anim.SetInteger("animation", 2);
-            canClick = true;
-        }
-        else if(anim.GetCurrentAnimatorStateInfo(0).IsName("2nd attack name") && noOfClicks == 2)  //If the second animation is still playing and only 2 clicks have happened, return to idle 
-        {        
-            anim.SetInteger("animation", 4);
-            canClick = true;
-            noOfClicks = 0;
-        }
-       else if (anim.GetCurrentAnimatorStateInfo(0).IsName("2nd attack name") && noOfClicks >= 3) //If the second animation is still playing and at least 3 clicks have happened, continue the combo
-        {           
-            anim.SetInteger("animation", 3);
-            canClick = true;           
-        }
-       else if (anim.GetCurrentAnimatorStateInfo(0).IsName("3rd attack name")) //Since this is the third and last animation, return to idle 
-        {          
-            anim.SetInteger("animation", 4);
-            canClick = true;
-            noOfClicks = 0;
-        }      
     }
+
+    public void ComboCheck() //"ComboCheck()" need to added in animation event of every attack. Event may added at the end of animation or as it necessary
+    {
+        canClick = false;
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(attack1) && noOfClicks == 1)
+        {
+            anim.SetInteger("ComboState", 4);
+            canClick = true;
+            noOfClicks = 0;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName(attack1) && noOfClicks >= 2)
+        {
+            anim.SetInteger("ComboState", 2);
+            canClick = true;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName(attack2) && noOfClicks == 2)
+        {
+            anim.SetInteger("ComboState", 4);
+            canClick = true;
+            noOfClicks = 0;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName(attack2) && noOfClicks >= 3)
+        {
+            anim.SetInteger("ComboState", 3);
+            canClick = true;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName(attack3))
+        {
+            anim.SetInteger("ComboState", 4);
+            canClick = true;
+            noOfClicks = 0;
+        }
+    }
+    
 }
